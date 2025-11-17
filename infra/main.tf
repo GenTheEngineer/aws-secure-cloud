7368300e0b6be5ba281bad6aa0e788c7dd55c2e5
Genesis Phillips <118129965+GenTheEngineer@users.noreply.github.com>
2025-11-17 16:04:41 +0100

    code corections

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,29 @@
-# Created a VPC
-# Defines the Virtual Private Cloud (VPC)
+# The main.tf defines the Virtual Private Cloud
 
-resource "aws_vpc" "main" {
-  cidr_block = "var.vpc_cidr"
+# Set my providers region and other options that applies to the provider itself
+provider "aws" {
+  region = "eu-north-1"
+}
+
+# Use data sources to query my cloud provider (AWS) for existing resources
+# Data sources help me keep my code dynamic by avoiding hardcoding values that may change over time
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# A resource block defines components for my infrastructure
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "Project VPC"
+    Name = "secure-cloud"
   }
 }
