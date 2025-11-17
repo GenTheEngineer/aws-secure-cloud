5b21f1201c3bdefb3f3daf9674859632f7524d98
Genesis Phillips <118129965+GenTheEngineer@users.noreply.github.com>
2025-11-17 15:15:14 +0100

    repo restructure

---
+++ infra/main.tf
@@ -0,0 +1,10 @@
+# Created a VPC
+# Defines the Virtual Private Cloud (VPC)
+
+resource "aws_vpc" "main" {
+  cidr_block = "var.vpc_cidr"
+
+  tags = {
+    Name = "Project VPC"
+  }
+}
---
+++ infra/providers.tf
@@ -0,0 +1,17 @@
+# Defines the required Terraform and AWS provider versions
+# and sets the deployment region to eu-north-1
+
+terraform {
+  required_version = ">= 1.13.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
